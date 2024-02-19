f40030ff90e6d818bf3c2b10600c9c13a95845b8
steven <sozastev@gmail.com>
2024-02-18 22:45:04 -0600

    ...

--- handler.py
+++ handler.py
@@ -6,19 +6,11 @@ from bson.json_util import dumps
 def get_inventario(event, context):
     uri = "mongodb+srv://sozastev:3UlUoyRsHdFJJMI8@link.k3fwk6i.mongodb.net/?retryWrites=true&w=majority"
     db = MongoClient(uri).get_database("link").get_collection("inventario")
-    try:
-        result = list(db.find(filter={}, projection={"_id": 0}))
+    result = list(db.find(filter={}))
 
-        # result = dumps(result)
-        # result.replace('\\', '')
-        result = json.loads(result)
-    except Exception as ex:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({
-                "error": "error"
-            })
-        }
+    result = dumps(result)
+    result.replace('\\', '')
+    result = json.loads(result)
 
     body = {
         "inventario": result
