d0f651bef9c7eceeedf22e43044a59fa21d8d262
Carlosaguirre5 <carlos199787@hotmail.es>
2024-02-18 22:38:59 -0600

    updates

--- handler.py
+++ handler.py
@@ -1,10 +1,30 @@
 import json
+from pymongo import MongoClient
+from bson.json_util import dumps
 
 
-def hello(event, context):
+def get_inventario(event, context):
+    uri = "mongodb+srv://sozastev:3UlUoyRsHdFJJMI8@link.k3fwk6i.mongodb.net/?retryWrites=true&w=majority"
+    db = MongoClient(uri).get_database("link").get_collection("inventario")
+    try:
+        result = list(db.find(filter={}, projection={"_id": 0}))
+
+        # result = dumps(result)
+        # result.replace('\\', '')
+        result = json.loads(result)
+    except Exception as ex:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "error": "error"
+            })
+        }
+
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "inventario": result
     }
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+    return {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
