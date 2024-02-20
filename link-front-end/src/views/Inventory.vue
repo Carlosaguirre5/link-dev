51dd366549d6b1f5787caa47d7c00337b3ca2744
steven <sozastev@gmail.com>
2024-02-19 23:43:56 -0600

    ...

---
+++ link-front-end/src/views/Inventory.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>API Data</h1>
+        <ul>
+            <li v-for="item in items" :key="item._id.$oid">{{ item.marca }}<div>{{ item.modelo }}</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: []
+        };
+    },
+    mounted() {
+        this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            try {
+                const response = await fetch('http://localhost:3000/dev/inventario');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                this.items = data.inventario; // Assuming the response is an array of items
+            } catch (error) {
+                console.error('There was a problem fetching the data:', error);
+            }
+        }
+    }
+};
+</script>
